71ee2e3e86c5f0fbbba35de78072c297043b0224
Paul Totzke <7072k3@gmail.com>
2021-03-27 02:01:33 -0500

    Problem 1: Reversort

--- Program.cs
+++ Program.cs
@@ -8,74 +8,63 @@ public class CodeJam2021
 {
     public static void Main(string[] args)
     {
-        new CodeJam2021().Solve(Vestigium);
+        Solve(Reversort, new StreamReader($"{nameof(Reversort)}.in"), new StreamWriter($"{nameof(Reversort)}.out"));
+        //Solve(Vestigium, System.Console.In, System.Console.Out);
     }
-
-    private void Solve(Func<int[][], int, string> problem)
+    public static void Solve(Func<int[], int, string> problem, TextReader reader, TextWriter writer)
     {
-        int cntCases = int.Parse(Console.ReadLine());
+        int cntCases = int.Parse(reader.ReadLine());
         for (int i = 1; i <= cntCases; ++i)
         {
-            int n = int.Parse(Console.ReadLine());
+            int n = int.Parse(reader.ReadLine());
 
-            int[][] m = new int[n][];
+            int[] m = new int[n];
+
+            string[] args = reader.ReadLine().Split(' ');
 
             for (int j = 0; j < n; j++)
             {
-                string[] args = Console.ReadLine().Split(' ');
-
-                int[] row = new int[args.Length];
-                for (int k = 0; k < row.Length; k++)
-                {
-                    row[k] = Int32.Parse(args[k]);
-                }
-
-                m[j] = row;
+                m[j] = int.Parse(args[j]);                
             }
 
             string answer = problem(m, n);
 
-            Console.WriteLine("Case #{0}: {1}", i, answer);
+            writer.WriteLine("Case #{0}: {1}", i, answer);
+            writer.Flush();
         }
+        writer.Close();
+        reader.Close();
     }
 
-
-    public static string Vestigium(int[][] m, int n)
+    /**
+     * Reversort(L):
+     *   for i := 1 to length(L) - 1
+     *       j := position with the minimum value in L between i and length(L), inclusive
+     *           Reverse(L[i..j])           
+     */
+    public static string Reversort(int[] list, int n)
     {
-        int trace = 0;
-        int r = 0; // rows w/ repeated elements
-        int c = 0; // cols w/ repeated elements
-
-        for (int i = 0; i < n; i++)
+        int cost = 0;
+        
+        for(int i = 0; i < list.Length-1; i++)
         {
-            HashSet<int> r_seen = new HashSet<int>();
-            HashSet<int> c_seen = new HashSet<int>();
-            for (int j = 0; j < n; j++)
+            int min = list[i];
+            int j = i;
+            for (int k = i+1; k < list.Length; k++)
             {
-                if (i == j)
-                {
-                    trace += m[i][j];
-                }
-
-                r_seen.Add(m[i][j]);
-
-
-                c_seen.Add(m[j][i]);
-
+                min = Math.Min(min, list[k]);
+                j = (min == list[k]) ? k : j;
             }
 
-            if (r_seen.Count != n)
+            int d = (j - i) / 2;
+            for(int k = 0; k < d; k++)
             {
-                r++;
+                (list[i+k], list[j-k]) = (list[j - k], list[i + k]);
             }
 
-            if (c_seen.Count != n)
-            {
-                c++;
-            }
+            cost += j - i + 1;
         }
 
-
-        return $"{trace} {r} {c}";
+        return $"{cost}";
     }
 }
